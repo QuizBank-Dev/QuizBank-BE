14dd70e558940a4951e735d8f91f7243887a72ab
Kim-HyounSuk <kkoor586@naver.com>
2025-03-11 08:25:57 +0900

    ✨ QuizAgain 스키마 정의

---
+++ src/modules/quiz/schema/quiz-again.schema.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { Quiz } from './quiz.schema';
+
+@Schema({ timestamps: true })
+export class QuizAgain extends Document {
+	@Prop({
+		type: [{ type: Types.ObjectId, ref: 'Quiz' }],
+		required: true,
+	})
+	quizzes: Types.ObjectId[] | Quiz[];
+
+	//// User 모델 완성 시 추가
+	// @Prop({
+	// 	type: Types.ObjectId,
+	// 	ref: 'User',
+	// 	required: true,
+	// })
+	// user: Types.ObjectId | User;
+}
