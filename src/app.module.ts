d9d25df171ca73c78c7f542c30124d4c681f3308
Kim-HyounSuk <kkoor586@naver.com>
2025-03-11 08:46:19 +0900

    ✨ Review 리소스 생성 및 DB 스키마 정의

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { DatabaseModule } from './database/database.module';
 import { QuizModule } from './modules/quiz/quiz.module';
 import { QuizbookModule } from './modules/quizbook/quizbook.module';
 import { CommentModule } from './modules/comment/comment.module';
+import { ReviewModule } from './modules/review/review.module';
 
 @Module({
-	imports: [ConfigModule, LoggerModule, DatabaseModule, QuizModule, QuizbookModule, CommentModule],
+	imports: [ConfigModule, LoggerModule, DatabaseModule, QuizModule, QuizbookModule, CommentModule, ReviewModule],
 	providers: [],
 })
 export class AppModule implements NestModule {
