10cceeaef9f61f5c1245449c2c2faabda3711f6f
Kim-HyounSuk <kkoor586@naver.com>
2025-03-25 10:53:14 +0900

    🚚 create-quizbook.dto.ts와 파일 분할

---
+++ src/modules/quiz/dto/create-quiz.dto.ts
@@ -0,0 +1,48 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+	ArrayMaxSize,
+	ArrayMinSize,
+	IsArray,
+	IsEnum,
+	IsNotEmpty,
+	IsOptional,
+	IsString,
+} from 'class-validator';
+import { QuizType } from '../schema/quiz.schema';
+
+export class createQuizDto {
+	@ApiProperty({
+		example: '퀵 정렬의 시간 복잡도는?',
+		description: '퀴즈 질문',
+	})
+	@IsString()
+	@IsNotEmpty()
+	question: string;
+
+	@ApiProperty({
+		example: 'O(n log n)',
+		description: '퀴즈 정답',
+	})
+	@IsString()
+	@IsNotEmpty()
+	answer: string;
+
+	@ApiProperty({
+		enum: QuizType,
+		example: QuizType.MULTIPLE_CHOICE,
+		description: '퀴즈 유형',
+	})
+	@IsEnum(QuizType)
+	type: QuizType;
+
+	@ApiProperty({
+		example: ['O(n log n)', 'O(n)', 'O(n^2)', 'O(1)'],
+		description: '객관식 문제의 선택지 (객관식일 경우 필수)',
+		required: false,
+	})
+	@IsOptional()
+	@IsArray()
+	@ArrayMaxSize(4)
+	@ArrayMinSize(4)
+	options?: string[];
+}
