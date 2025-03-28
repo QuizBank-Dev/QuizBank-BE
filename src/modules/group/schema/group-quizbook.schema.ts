2fe62f61dec44d533285ba3e739121b084fbac22
Pa55er <tjrwns2715@naver.com>
2025-03-29 02:02:35 +0900

    ⚰️ 데코레이터 제거

--- src/modules/group/schema/group-quizbook.schema.ts
+++ src/modules/group/schema/group-quizbook.schema.ts
@@ -1,27 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { ApiProperty } from '@nestjs/swagger';
 import { Document, Types } from 'mongoose';
 import { Quizbook } from 'src/modules/quizbook/schema/quizbook.schema';
 
 @Schema({ timestamps: true })
 export class GroupQuizbook extends Document {
-	@ApiProperty({
-		example: '65e8a5d6fc13ae5e7f000002',
-		description: '그룹 선정 문제집 UUID',
-	})
-	_id: string;
-
-	@ApiProperty({
-		example: '65e8a5d6fc13ae5e7f000002',
-		description: '문제집 UUID',
-	})
 	@Prop({ type: Types.ObjectId, ref: 'Quizbook', required: true })
 	workbookId: Types.ObjectId | Quizbook;
 
-	@ApiProperty({
-		example: '2025-04-01T00:00:00Z',
-		description: '그룹에서의 문제집 마감일',
-	})
 	@Prop({
 		type: Date,
 		required: true,
