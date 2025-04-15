62499b4f8d5c062ce65283933bb6bf632e1705a6
Kim-HyounSuk <kkoor586@naver.com>
2025-04-15 20:43:25 +0900

    ✨ 유저 Flag 추가 메서드 작성

--- src/modules/quiz/quiz.service.ts
+++ src/modules/quiz/quiz.service.ts
@@ -1,9 +1,15 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { QuizRepository } from './quiz.repository';
+import { Quiz } from './schema/quiz.schema';
+import { LikeRepository } from '../like/like.repository';
+import { Types } from 'mongoose';
 
 @Injectable()
 export class QuizService {
-	constructor(private readonly quizRepo: QuizRepository) {}
+	constructor(
+		private readonly quizRepo: QuizRepository,
+		private readonly likeRepo: LikeRepository,
+	) {}
 
 	// 특정 Quiz 조회
 	async getQuizDetail(quizId: string) {
@@ -15,5 +21,30 @@ export class QuizService {
 			);
 
 		return quiz;
+	}
+
+	// isLiked 필드 추가 메서드
+	async addUserFlagsToQuiz(
+		data: Quiz | Quiz[],
+		userId: string,
+		likedIdList?: string[],
+	) {
+		const isArray = Array.isArray(data);
+		const quizList = isArray ? data : [data];
+
+		const likedSet = likedIdList
+			? new Set(likedIdList)
+			: new Set(await this.likeRepo.findQuizLikeIdListByOwner(userId));
+
+		const result = quizList.map((quiz) => {
+			const id = (quiz._id as Types.ObjectId).toString();
+
+			return {
+				...quiz,
+				...(likedSet && { isLiked: likedSet.has(id) }),
+			};
+		});
+
+		return isArray ? result : result[0];
 	}
 }
