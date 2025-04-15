2d233e43a210f252ed78c22a44c154979e3ca3ee
Kim-HyounSuk <kkoor586@naver.com>
2025-04-15 22:28:09 +0900

    🐛 기존 로직 제거로 인한 주석처리

--- src/modules/user/user.example.ts
+++ src/modules/user/user.example.ts
@@ -1,4 +1,4 @@
-import { quizbookBaseExample } from '../quizbook/quizbook.example';
+// import { getQuizbookListEx } from '../quizbook/quizbook.example';
 
 export const meExample = {
 	_id: '65e8a5d6fc13ae5e7f000001',
@@ -17,7 +17,9 @@ export const otherExample = {
 	introduce: '',
 	experience: 0,
 	follower: ['65e8a5d6fc13ae5e7f000001'],
+
+	// 엔드포인트가 따로 파진 관계로 주석 처리 했습니다.
 	// TODO Example 수정 필요
-	quizbook: [{ ...quizbookBaseExample, author: '65e8a5d6fc13ae5e7f000011' }],
-	record: [],
+	// quizbook: getQuizbookListEx,
+	// record: [],
 };
